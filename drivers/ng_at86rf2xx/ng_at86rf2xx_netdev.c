tanupoo/RIOT

ng_netconf: ng_at86rf2xx: set retransmissions
Introduces a netconf option to configure the maximum amount of
retransmissions and implements this for the at86rf2xx radios.

drivers/ng_at86rf2xx/ng_at86rf2xx_netdev.c
@@ -445,6 +445,13 @@ static int _get(ng_netdev_t *device, ng_netconf_opt_t opt,
             }
             return sizeof(ng_netconf_enable_t);
 
+        case NETCONF_OPT_RETRANS:
+            if (max_len < sizeof(uint8_t)) {
+                return -EOVERFLOW;
+            }
+            *((uint8_t *)val) = ng_at86rf2xx_get_txpower(dev);
+            return sizeof(uint8_t);
+
         case NETCONF_OPT_PROMISCUOUSMODE:
             if (dev->options & NG_AT86RF2XX_OPT_PROMISCUOUS) {
                 *((ng_netconf_enable_t *)val) = NETCONF_ENABLE;
@@ -576,6 +583,13 @@ static int _set(ng_netdev_t *device, ng_netconf_opt_t opt,
             ng_at86rf2xx_set_option(dev, NG_AT86RF2XX_OPT_AUTOACK,
                                     ((bool *)val)[0]);
             return sizeof(ng_netconf_enable_t);
+
+        case NETCONF_OPT_RETRANS:
+            if (len > sizeof(uint8_t)) {
+                return -EOVERFLOW;
+            }
+            ng_at86rf2xx_set_max_retries(dev, *((uint8_t *)val));
+            return sizeof(uint8_t);
 
         case NETCONF_OPT_PRELOADING:
             ng_at86rf2xx_set_option(dev, NG_AT86RF2XX_OPT_PRELOADING,

laparmakerli/riot-upstream

drivers/ng_at86rf2xx/ng_at86rf2xx_netdev.c
@@ -445,6 +445,13 @@ static int _get(ng_netdev_t *device, ng_netconf_opt_t opt,
             }
             return sizeof(ng_netconf_enable_t);
 
+        case NETCONF_OPT_RETRANS:
+            if (max_len < sizeof(uint8_t)) {
+                return -EOVERFLOW;
+            }
+            *((uint8_t *)val) = ng_at86rf2xx_get_max_retries(dev);
+            return sizeof(uint8_t);
+
         case NETCONF_OPT_PROMISCUOUSMODE:
             if (dev->options & NG_AT86RF2XX_OPT_PROMISCUOUS) {
                 *((ng_netconf_enable_t *)val) = NETCONF_ENABLE;
@@ -576,6 +583,13 @@ static int _set(ng_netdev_t *device, ng_netconf_opt_t opt,
             ng_at86rf2xx_set_option(dev, NG_AT86RF2XX_OPT_AUTOACK,
                                     ((bool *)val)[0]);
             return sizeof(ng_netconf_enable_t);
+
+        case NETCONF_OPT_RETRANS:
+            if (len > sizeof(uint8_t)) {
+                return -EOVERFLOW;
+            }
+            ng_at86rf2xx_set_max_retries(dev, *((uint8_t *)val));
+            return sizeof(uint8_t);
 
         case NETCONF_OPT_PRELOADING:
             ng_at86rf2xx_set_option(dev, NG_AT86RF2XX_OPT_PRELOADING,