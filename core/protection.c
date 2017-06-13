d66e3e738545c87e7770988633f407561fe63215
lars <l.parmakerli@googlemail.com>
2017-06-13 21:29:56 +0200

    fix bracket bug in __loadcheck

--- core/protection.c
+++ core/protection.c
@@ -40,7 +40,7 @@ void __loadcheck(void* pointer, __int64_t access_size) {
 
     // Load access in R1 - allowed
     
-    if (ptr < upper_stack_bound && ptr > (lower_stack_bound-32){
+    if (ptr < upper_stack_bound && ptr > (lower_stack_bound-32)){
         in_stack += 1;
         return;
     }
